f415b5caffb3fd0c2794a3b93c5236ef51a3ebad
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-30 15:54:03 +0200

    Ajout de FormsModule pour pouvoir utiliser le ngModel utilisation du ngModel bouton addpokemon disabled si l'input est vide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { PokemonListComponent } from './components/pokemon-list/pokemon-list.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonListComponent implements OnInit {
   nbCaught = 0;
+  pokemonName = '';
 
   constructor() {}
 
