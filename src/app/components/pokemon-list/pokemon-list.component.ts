a30c2ebe1b9d044991f55d79918ad46a35131be4
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-30 15:05:20 +0200

    Created pokemon list and pokemon item 2 buttons to add or remove pokemon (increase or decrease nbCought)

---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.scss'],
+})
+export class PokemonListComponent implements OnInit {
+  nbCaught = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addPokemon() {
+    this.nbCaught += 1;
+  }
+
+  removePokemon() {
+    if (this.nbCaught <= 0) return;
+    this.nbCaught -= 1;
+  }
+}
